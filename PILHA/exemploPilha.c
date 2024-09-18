36f2bb02520ad419865dd9ec4b4fd25fb0d0994a
Yan da Costa Fernandes Gomes <143640083+yancfgomes@users.noreply.github.com>
2024-09-18 23:33:36 +0000

    Refatorar código do estacionamento
    Remover arquivos desnecessários e organizar código do estacionamento, seguindo as diretrizes do exercício proposto.

--- PILHA/exemploPilha.c
+++ PILHA/exemploPilha.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
+#include <stdbool.h>
 #define TAMANHO_PILHA 4
-struct Pilha{
+typedef struct Pilha{
 	int dados[TAMANHO_PILHA];//vetor
 	int topo;//controle do topo da pilha
-};
-
-Pilha p; //variavel pilha
+}Pilha;
 
 /*
 * funcao para inicialização da pilha
@@ -62,4 +61,16 @@ void exibirTudo(Pilha *p){
 		}while(auxTopo >= 0);
 		printf("Fim da pilha!\n");
 	}
+}
+
+int main(){
+    Pilha p1, p2; //variavel pilha
+    inicializar(&p1);
+    inicializar(&p2);
+    empilhar(&p1, 5);
+    empilhar(&p1, 8);
+    empilhar(&p2, 3);
+    empilhar(&p1, 6);
+    exibirTudo(&p1);
+    exibirTudo(&p2);
 }
